3f91ab8571565a3216965b43b41d21a338f6a5bd
stevenkassof <47457431+stevenkassof@users.noreply.github.com>
2019-05-01 12:59:02 -0400

    Update QueueTest.java

--- src/test/java/sampleQueue/QueueTest.java
+++ src/test/java/sampleQueue/QueueTest.java
@@ -2,7 +2,10 @@ package sampleQueue;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.NoSuchElementException;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,15 +22,10 @@ class QueueTest {
 	 */
 
 	private static final String SOME_ITEM = "some-content";
+	private static final String SOME_OTHER_ITEM = "some-other-content";
 	private Queue<String> q;
 
 
-	@Test
-	@Disabled
-	@DisplayName("is instantiated with new Queue()")
-	void isInstantiatedWithNew() {
-		new Queue<>();
-	}
 
 	@BeforeEach
 	void init() {
@@ -40,14 +38,102 @@ class QueueTest {
 		assertTrue(q.isEmpty());
 	}
 
-	//Example of Wrong Test! 
+
 	@Test
-	@Disabled
-	@DisplayName("Verify Queue isEmpty returns false when queue is not empty")
-	void isEmptyShouldGiveFalseWhenQueueIsNotEmpty() {
-		this.q.enqueue(SOME_ITEM);
-		assertFalse(q.isEmpty());
+	@DisplayName("Verify Queue isEmpty when queue is initialized (second constructor), check size")
+	void isEmptyShouldGiveTrueOnQueueInit2() {
+
+		this.q = new Queue<String>(9);
+		assertTrue(q.isEmpty());
+		assertEquals(q.size(),0);
+		q.enqueue(SOME_ITEM);
+		assertEquals(q.size(),1);
 	}
+	
+
+	@Test
+	@DisplayName("Verify enqueue/dequeue/peek works for single object")
+	void singleObjectEnqueueTest() {
+
+		q.enqueue(SOME_ITEM);
+		assertFalse(q.isEmpty());
+		assertEquals(q.peek(),SOME_ITEM);
+		assertEquals(q.dequeue(),SOME_ITEM);
+		assertThrows(NoSuchElementException.class, () -> {q.peek();});
+		assertThrows(NoSuchElementException.class, () -> {q.dequeue();});
+		}
+	
+	@Test
+	@DisplayName("Verify enqueue/dequeue/peek works for two objects")
+	void twoObjectQueueTest() {
+
+		q.enqueue(SOME_ITEM);
+		q.enqueue(SOME_OTHER_ITEM);
+		assertEquals(q.peek(),SOME_ITEM);
+		assertEquals(q.dequeue(),SOME_ITEM);
+		assertEquals(q.peek(),SOME_OTHER_ITEM);
+		assertEquals(q.dequeue(),SOME_OTHER_ITEM);
+		assertThrows(NoSuchElementException.class, () -> {q.peek();});
+		assertThrows(NoSuchElementException.class, () -> {q.dequeue();});
+	}
+	
+	@Test
+	@DisplayName("Verify enqueue/dequeue/peek works for two objects")
+	void threeObjectQueueTest() {
+
+		q.enqueue(SOME_ITEM);
+		q.enqueue(SOME_OTHER_ITEM);
+		q.enqueue(SOME_ITEM);
+		assertEquals(q.peek(),SOME_ITEM);
+		assertEquals(q.dequeue(),SOME_ITEM);
+		assertEquals(q.peek(),SOME_OTHER_ITEM);
+		assertEquals(q.dequeue(),SOME_OTHER_ITEM);
+		assertEquals(q.peek(),SOME_ITEM);
+		assertEquals(q.dequeue(),SOME_ITEM);
+		assertThrows(NoSuchElementException.class, () -> {q.peek();});
+		assertThrows(NoSuchElementException.class, () -> {q.dequeue();});
+	}
+	
+
+	@Test
+	@DisplayName("Verify enqueue does not add objects past maximum")
+	void fullTest() {
+
+		this.q = new Queue<String>(3);
+
+		q.enqueue(SOME_ITEM);
+		q.enqueue(SOME_ITEM);
+		q.enqueue(SOME_ITEM);
+		assertThrows(Exception.class, () -> {q.enqueue(SOME_ITEM);});
+		}
+	
+	@Test
+	@DisplayName("Verify toString works")
+	void toStringTest() {
+
+		q.enqueue(SOME_ITEM);
+		assertEquals(q.toString(),SOME_ITEM+" ");
+		q.enqueue(SOME_OTHER_ITEM);
+		assertEquals(q.toString(),SOME_ITEM+" "+SOME_OTHER_ITEM+" ");
+	}
+	
+
+	
+	@Test
+	@DisplayName("Verify removeAll works")
+	void removeAllTest() {
+
+		q.enqueue(SOME_ITEM);
+		q.enqueue(SOME_OTHER_ITEM);
+		q.enqueue(SOME_OTHER_ITEM);
+		q.enqueue(SOME_OTHER_ITEM);
+		q.removeAll();
+		assertTrue(q.isEmpty());
+	}
+	
+
+	
+
 
 	
 }
